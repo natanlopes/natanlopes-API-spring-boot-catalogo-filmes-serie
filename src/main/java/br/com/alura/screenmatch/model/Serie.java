8caa32145244a6351838f6cdd7aa9c82cc3b3e36
Natan Lopes <natan1918@gmail.com>
2024-12-25 21:40:21 -0300

    Mapeando as entidades

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -4,11 +4,25 @@ import java.util.OptionalDouble;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
 import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "series")
 public class Serie {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+	@Column(name = "nomeDaSerie")
 	private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
@@ -27,6 +41,18 @@ public class Serie {
 
 
     }
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 
 
