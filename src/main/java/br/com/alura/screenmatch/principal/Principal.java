731df89c81fc7580ea726aa97f9915ca1811a808
Natan Lopes <natan1918@gmail.com>
2025-01-28 20:53:31 -0300

    Buscas com múltiplos critérios

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -44,6 +44,7 @@ public class Principal {
                 2 - Buscar episódios
                 3 - Listar séries buscadas
                 4 - Buscar série por título
+                5 - Buscar séries por ator
                 0 - Sair                                 
                 """;
 
@@ -64,6 +65,10 @@ public class Principal {
             case 4:
                 buscarSeriePorTitulo();
                 break;
+            case 5:
+                buscarSeriesPorAtor();
+                break;
+
             case 0:
                 System.out.println("Saindo...");
                 break;
@@ -73,7 +78,19 @@ public class Principal {
     	}
     }
 
-    private void buscarSeriePorTitulo() {
+    private void buscarSeriesPorAtor() {
+		// TODO Auto-generated method stub
+    	System.out.println("Qual o nome para busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        seriesEncontradas.forEach(s ->
+                        System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+	}
+
+	private void buscarSeriePorTitulo() {
 		// TODO Auto-generated method stub
     	System.out.println("Escolha um série pelo nome: ");
     	var nomeSerie = leitura.nextLine();
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,7 @@ import br.com.alura.screenmatch.model.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
 	 Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+
+	List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
 }
