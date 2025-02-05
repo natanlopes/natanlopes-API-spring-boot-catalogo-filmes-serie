40a11cec07504feed295c41c2694fc506f27ba72
Natan Lopes <natan1918@gmail.com>
2025-02-05 20:50:19 -0300

    Conhecendo o JPQL

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Episodio;
@@ -46,6 +47,8 @@ public class Principal {
                 4 - Buscar série por título
                 5 - Buscar séries por ator
                 6 - Top 5 Séries
+                7 - Buscar séries por categoria
+                8 - Filtrar séries  
                 0 - Sair                                 
                 """;
 
@@ -72,7 +75,12 @@ public class Principal {
             case 6:
             	buscarTop5Series();
                 break;
-
+            case 7:
+                buscarSeriesPorCategoria();
+                break;
+            case 8:
+            	filtrarSeriesPorTemporadaEAvaliacao();
+                break;
             case 0:
                 System.out.println("Saindo...");
                 break;
@@ -83,6 +91,30 @@ public class Principal {
     }
 
 	// código omitido
+
+	//Código omitido
+
+	private void filtrarSeriesPorTemporadaEAvaliacao(){
+	        System.out.println("Filtrar séries até quantas temporadas? ");
+	        var totalTemporadas = leitura.nextInt();
+	        leitura.nextLine();
+	        System.out.println("Com avaliação a partir de que valor? ");
+	        var avaliacao = leitura.nextDouble();
+	        leitura.nextLine();
+	        List<Serie> filtroSeries = repositorio.seriesPorTemporadaEAValiacao(totalTemporadas, avaliacao);
+	        System.out.println("*** Séries filtradas ***");
+	        filtroSeries.forEach(s ->
+	                        System.out.println(s.getTitulo() + "  - avaliação: " + s.getAvaliacao()));
+	}
+
+	private void buscarSeriesPorCategoria() {
+        System.out.println("Deseja buscar séries de que categoria/gênero? ");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + nomeGenero);
+        seriesPorCategoria.forEach(System.out::println);
+}
 
 	private void buscarTop5Series() {
 	        List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
@@ -14,4 +16,14 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
 	List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
 
 	List<Serie> findTop5ByOrderByAvaliacaoDesc();
+	
+	List<Serie> findByGenero(Categoria categoria);
+
+
+	//Código omitido
+
+	@Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
+	List<Serie> seriesPorTemporadaEAValiacao(int totalTemporadas, double avaliacao);
+
+	//Código omitido
 }
